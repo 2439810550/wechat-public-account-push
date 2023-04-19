75b7a5e2d7fcb2d008d3e1b2b65a6bd0d75c9a33
ZhangJiuKe <103210522+2439810550@users.noreply.github.com>
2023-04-19 12:19:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx87c6927317e395f1',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'c804cd0e9eff96f1b1ade955bbc0f504',
+  APP_SECRET: '',
 
   PROVINCE: '河南',
   CITY: '南阳',
@@ -51,13 +51,13 @@ USE_PASSAGE: 'push-plus',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0001',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocawh5ve3n4oAOPZussvC4mzgMjI',
+      id: '85f1d26f4d124e7abf3d94143e67ea9b',
     }
   ],
 
