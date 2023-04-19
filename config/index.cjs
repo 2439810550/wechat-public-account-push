f0a7cfa59137b0e59a7297e559b1bfb79904353b
ZhangJiuKe <103210522+2439810550@users.noreply.github.com>
2023-04-19 11:54:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,12 +6,12 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+USE_PASSAGE: 'push-plus',
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx87c6927317e395f1',
+  APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'c804cd0e9eff96f1b1ade955bbc0f504',
+  APP_SECRET: '',
 
   PROVINCE: '河南',
   CITY: '南阳',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ocawh5qC0W0CQPjeUjlt2JHYQ1m0','ocawh5ve3n4oAOPZussvC4mzgMjI',
+      id: '85f1d26f4d124e7abf3d94143e67ea9b',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'pI0jwWHdlPumB6kJd-iWVzrJCYgUqljz0Sw8O4ZERys',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-04',
       festivals: [
