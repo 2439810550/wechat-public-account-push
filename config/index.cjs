040697a79b967642f4c0951759173357220f97fe
ZhangJiuKe <103210522+2439810550@users.noreply.github.com>
2023-04-30 20:47:24 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ USE_PASSAGE: 'push-plus',
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '85f1d26f4d124e7abf3d94143e67ea9b',
+      id: '85f1d26f4d124e7abf3d94143e67ea9b','d46c2c9f39014e62b14a5ea11a66dcaa',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ USE_PASSAGE: 'push-plus',
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '85f1d26f4d124e7abf3d94143e67ea9b',
+      id: '85f1d26f4d124e7abf3d94143e67ea9b','d46c2c9f39014e62b14a5ea11a66dcaa',
     }
   ],
 
